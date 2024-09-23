9f9b44bc0d1d9720bc8123e1fc0a811f463ab22c
Nkhaoula <khaoula.noumi.fr@gmail.com>
2024-09-23 16:32:22 +0200

    mise à jour «25-supprimer-article+ »

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="text-center text-light myFooter mt-5">
     <div class="text-center p-3">
-        © Copyright : nanaplouf <?php echo date("Y")?>
+        © Copyright : Khaoual NOUMI <?php echo date("Y")?>
     </div>
 </footer>
 </body>
